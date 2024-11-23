ad352d2f486cb067874cd88a5e82312310306f17
tomkel <520225+tomkel@users.noreply.github.com>
2024-11-22 20:12:04 -0800

    refactor: remove old geoloc code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
-  // https://github.com/dimfeld/svelte-maplibre/blob/next/src/lib/DefaultMarker.svelte
   // https://github.com/dimfeld/svelte-maplibre/blob/next/src/lib/GeolocateControl.svelte
   // https://maplibre.org/maplibre-gl-js/docs/API/classes/GeolocateControl
   import {
     MapLibre,
-    DefaultMarker,
     FullscreenControl,
     NavigationControl,
     GeolocateControl,
     ScaleControl,
   } from 'svelte-maplibre'
-  import maplibregl from 'maplibre-gl'
+  import type maplibregl from 'maplibre-gl'
 
   type GeoEvent =
     | 'trackuserlocationstart'
@@ -30,28 +28,13 @@
     'error',
     'outofmaxbounds',
   ]
-  const lngLat = new maplibregl.LngLat(-20, 20)
 
-  let lng = $state(0)
-  let lat = $state(0)
-
-  const success = (position: GeolocationPosition) => {
-    console.log('geoloc success', position)
-    lng = position.coords.longitude
-    lat = position.coords.latitude
-  }
-  const error = (err: GeolocationPositionError) => {
-    console.error('geoloc error', err)
-    throw new Error(err.message)
-  }
   const options: PositionOptions = {
     enableHighAccuracy: false, // true == GPS, false == Wifi-location
     maximumAge: 60000, // in ms
     // timeout: 60000,
     timeout: 5000, // in ms
   } satisfies PositionOptions
-  // const id = navigator.geolocation.watchPosition(success, error, options)
-  // return () => navigator.geolocation.clearWatch(id)
 
   let control = $state<maplibregl.GeolocateControl | undefined>(undefined)
   $effect(() => {
@@ -82,7 +65,6 @@
   style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
   attributionControl={false}
 >
-  <DefaultMarker {lngLat} draggable />
   <NavigationControl />
   <GeolocateControl
     fitBoundsOptions={{ maxZoom: 4 }}
